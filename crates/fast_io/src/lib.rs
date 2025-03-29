12ac25a9f7007c04b3949bcfb71fae07d4adfa5f
qdot3 <quantum.engine.55g@gmail.com>
2025-03-29 23:39:29 +0900

    modify

--- crates/fast_io/src/lib.rs
+++ crates/fast_io/src/lib.rs
@@ -1,5 +1,3 @@
 mod from_bytes;
-mod into_bytes;
 
 pub use from_bytes::FromBytes;
-pub use into_bytes::IntoBytes;
--- examples/ex-many_aplusb.rs
+++ examples/ex-many_aplusb.rs
@@ -1,7 +1,8 @@
 // verification-helper: PROBLEM https://judge.yosupo.jp/problem/many_aplusb
 
-use fast_io::{FromBytes, IntoBytes};
+use fast_io::FromBytes;
 use std::io::{stdout, BufWriter, Read, Write};
+use atoi::atoi;
 
 fn main() {
     let mut buf_r = Vec::new();
@@ -10,14 +11,13 @@ fn main() {
         .split(|b| b.is_ascii_whitespace())
         .filter(|bytes| !bytes.is_empty())
         .skip(1)
-        .map(|bytes| u64::from_bytes(bytes).unwrap());
+        .map(|bytes| atoi::<u64>(bytes).unwrap());
+        // .map(|bytes| u64::from_bytes(bytes).unwrap());
 
     let mut buf_w = BufWriter::new(stdout().lock());
     while let Some(x) = num.next() {
         let y = num.next().unwrap();
 
-        buf_w.write(&(x + y).into_bytes()).unwrap();
-        buf_w.write(b"\n").unwrap();
-        // writeln!(&mut buf_w, "{}", x + y).unwrap();
+        writeln!(&mut buf_w, "{}", x + y).unwrap();
     }
 }
