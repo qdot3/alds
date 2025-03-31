16402308480a6f02a9519cadbcbf9c034481ff7a
qdot3 <quantum.engine.55g@gmail.com>
2025-03-31 20:40:05 +0900

    fix

--- crates/fast_io/examples/many_aplusb_128bit.rs
+++ crates/fast_io/examples/many_aplusb_128bit.rs
@@ -5,12 +5,12 @@ use std::io::{stdin, stdout, BufWriter};
 use fast_io::{FastInput, FastWrite, FromBytes};
 
 fn main() {
-    let mut buf_r = FastInput::<{ 8 * 1024 }, _>::new(stdin().lock());
-    let n = usize::from_bytes(buf_r.next_token().unwrap().as_slice()).unwrap();
+    let mut fast_in = FastInput::<{ 8 * 1024 }, _>::new(stdin().lock());
+    let n = usize::from_bytes(fast_in.next_token().unwrap().as_slice()).unwrap();
     let mut buf_w = BufWriter::new(stdout().lock());
     for _ in 0..n {
-        let x = u64::from_bytes(buf_r.next_token().unwrap().as_slice()).unwrap();
-        let y = u64::from_bytes(buf_r.next_token().unwrap().as_slice()).unwrap();
+        let x = i128::from_bytes(fast_in.next_token().unwrap().as_slice()).unwrap();
+        let y = i128::from_bytes(fast_in.next_token().unwrap().as_slice()).unwrap();
 
         buf_w.fast_writeln(x + y).unwrap();
     }
